41504ae867920e875dce637728dfef7f9637bc05
Kenan AKSOY <kenannaksoyy@gmail.com>
2023-02-16 17:07:36 +0300

    Kenan AKSOY

--- index.js
+++ index.js
@@ -8,7 +8,13 @@
  */
 function nesneyiTrimle(obj) {
   // ✨ kodlar buraya
+  const trimObject = {};
+  for(let key in obj){
+    trimObject[key] = obj[key].trim();
+  }
+  return trimObject;
 }
+console.log("nesneyiTrimle",nesneyiTrimle({ isim: '  jane  ' }));
 
 /**
  * [Görev 2] verileniTrimle propları string olan bir nesne alır ve gönderilen propu trimler.
@@ -20,7 +26,13 @@ function nesneyiTrimle(obj) {
  */
 function verileniTrimle(obj, prop) {
   // ✨ kodlar buraya
+  const trimObject = {};
+  for(let key in obj){
+    key == prop ? trimObject[key] = obj[key].trim() : trimObject[key] = obj[key];
+  }
+  return trimObject;
 }
+console.log("verileniTrimle",verileniTrimle({ isim: '  jane  ' , yas: ' 34 '}, 'isim'));
 
 /**
  * [Görev 3] enBuyukTamsayiyiBul bir dizi nesne içinde bulunan tamsayılardan en büyük olanı bulur { tamsayi: 1 }
@@ -32,7 +44,13 @@ function verileniTrimle(obj, prop) {
  */
 function enBuyukTamsayiyiBul(tamsayilar) {
   // ✨ kodlar buraya
+  let max = tamsayilar[0].tamsayi;
+  tamsayilar.forEach(t => {
+    t.tamsayi > max && (max = t.tamsayi);
+  });
+  return max;
 }
+console.log("enBuyukTamsayiyiBul",enBuyukTamsayiyiBul([{ tamsayi: 1 }, { tamsayi: 3 }, { tamsayi: 2 }]));
 
 function Sayici(ilkSayi) {
   /**
@@ -55,10 +73,18 @@ function Sayici(ilkSayi) {
    * sayac.asagiSay() // 0 döndürür
    * sayac.asagiSay() // 0 döndürür
    */
+  this.ilkSayi = ilkSayi;
   this.asagiSay = () => {
     // ✨ kodlar buraya
+    return this.ilkSayi <= 0 ?  0 : this.ilkSayi--; //dondurme -- operatoru değer dondurultem sonra değişim sağlar
+    //this.ilkSayi = this.ilkSayi -1 an değişir
   }
 }
+const sayici = new Sayici(5);
+for(let i = 0; i<=6 ; i++){
+  console.log(`asagiSay${i}`,sayici.asagiSay());
+};
+
 
 function Mevsimler() {
   /**
@@ -79,12 +105,20 @@ function Mevsimler() {
    * mevsimler.sonraki() // "ilkbahar" döndürür
    * mevsimler.sonraki() // "yaz" döndürür
    */
+  this.index = 0;
+  this.mevsimler = ["ilkbahar", "yaz", "sonbahar", "kış"];
   this.sonraki = () => {
     // ✨ kodlar buraya
+    this.index = (this.index +1) % this.mevsimler.length;
+    return this.mevsimler[this.index];
   }
 }
+const mevsimler = new Mevsimler();
+for(let i=0 ; i<8;i++){
+  console.log(`mevsimler${i}`,mevsimler.sonraki());
+}
 
-function Araba(/*kodlar buraya */) {
+function Araba(/*kodlar buraya */isim,depo,kml) {
   /**
    * [Görev 6A] Araba 3 argüman alarak bir araba nesnesi oluşturur
    * @param {string} isim - arabanın ismi
@@ -93,9 +127,11 @@ function Araba(/*kodlar buraya */) {
    */
  
     this.odometer = 0 // araba 0 kilometrede yüklenecek
-    this.depo = depoBenzin // araba full depoyla yüklenecek
+    this.depo = depo // araba full depoyla yüklenecek
     // ✨ gerekli propları ekleyin
-
+    this.isim = isim;
+    this.kml = kml;
+    this.depoKapasite = depo;
   
 
   /**
@@ -113,7 +149,17 @@ function Araba(/*kodlar buraya */) {
    */
   this.sur = (gidilecekyol) => {
     // ✨ kodlar buraya
+    const yolKapasite = this.depo * this.kml;
+    if(gidilecekyol<=yolKapasite){
+      this.odometer = this.odometer + gidilecekyol;
+      this.depo = this.depo - gidilecekyol/this.kml
+      return this.odometer;
+    }
+    this.depo = 0;
+    this.odometer = this.odometer + yolKapasite;
+    return this.odometer;
   }
+
 
   /**
    * [Görev 6C] Depoya benzin ekleme
@@ -128,9 +174,27 @@ function Araba(/*kodlar buraya */) {
    */
   this.benzinal = (litre) => {
     // ✨ kodlar buraya
+    const depoBosLitre = this.depoKapasite - this.depo;
+    let kapasiteKm;
+    if(litre <= depoBosLitre){
+      this.depo = this.depo + litre;
+      kapasiteKm = this.depo * this.kml;
+      return kapasiteKm;
+    }
+    this.depo = this.depoKapasite;
+    kapasiteKm = this.depo * this.kml;
+    return kapasiteKm;
   }
 }
-
+const focus = new Araba('focus', 20, 30)
+const kmArr = [100,100,100,200,200];
+kmArr.forEach(km => {
+  console.log("sur", focus.sur(km));
+})
+const c3 = new Araba('c3', 20, 30)
+console.log("c3 sur",c3.sur(600)) // 600 döndürür
+console.log("c3 sur",c3.sur(1)) // 600 döndürür (depo boş olduğundan yol gidilemedi)
+console.log("c3 benzinal",c3.benzinal(99) )// 600 döndürür (depo yalnızca 20 litre alabiliyor)
 /**
  * [Görev 7] Bir sayının çift olup olmadığını asenkron olarak çözümler
  * @param {number} sayi - kontrol edilecek sayı
@@ -145,8 +209,22 @@ function Araba(/*kodlar buraya */) {
  * })
  */
 function asenkronCiftSayi(sayi) {
-  // ✨ implement
+  return new Promise(res => {
+    if(sayi%2 == 0){
+      res(true)
+    }
+    res(false)
+    // Promise bekle beni söz
+  });
 }
+asenkronCiftSayi(1).then(res => {
+  console.log("asenkronCiftSayi1",res);
+});
+asenkronCiftSayi(2).then(res => {
+  console.log("asenkronCiftSayi2",res); 
+});
+
+
 
 module.exports = {
   nesneyiTrimle,
--- index.test.js
+++ index.test.js
@@ -11,12 +11,28 @@ describe('[Görev 1] nesneyiTrimle', () => {
 })
 
 describe('[Görev 2] verileniTrimle', () => {
-  // test('[3] verilen propu trimliyor', () => {})
-  // test('[4] verilen dışındaki proplar trimlenmeden döndürülüyor', () => {})
+  test('[3] verilen propu trimliyor', () => {
+    const inputObj = { foo: '  foo ', bar: 'bar ', baz: ' baz' }
+    const expected = { foo: 'foo', bar: 'bar ', baz: ' baz' }
+    const actual = utils.verileniTrimle(inputObj,'foo')
+    expect(actual).toEqual(expected);
+  })
+  test('[4] verilen dışındaki proplar trimlenmeden döndürülüyor', () => {
+    const input = { foo: '  foo ', bar: 'bar ', baz: ' baz' }
+    const expected = 'bar '
+    const actual = utils.verileniTrimle(input,'foo')
+    expect(actual.bar).toBe(expected);
+  })
+  //toBe değersel toEqual nesnesel (stackorflow)
 })
 
 describe('[Görev 3] enBuyukTamsayiyiBul', () => {
-  // test('[5] bir dizi nesne içindeki en büyük tamsayiyi döndürüyor { tamsayi: 2 }', () => {})
+  test('[5] bir dizi nesne içindeki en büyük tamsayiyi döndürüyor { tamsayi: 2 }', () => {
+    const input = [{ tamsayi: 1 }, { tamsayi: 2 }, { tamsayi: 3 }];
+    const actual = utils.enBuyukTamsayiyiBul(input);
+    expect(actual).toBe(3);
+  })
+    
 })
 
 describe('[Görev 4] Sayici', () => {
@@ -24,9 +40,22 @@ describe('[Görev 4] Sayici', () => {
   beforeEach(() => {
     sayici = new utils.Sayici(3) // her test yeni bir sayı ile başlatılıyor
   })
-  // test('[6] sayici.asagiSay ilk çağırılışında başlangıç sayışını yapıyor', () => {})
-  // test('[7] sayici.asagiSay İKİNCİ çağırılışında başlangıç eksi 1 sayıyor', () => {})
-  // test('[8] sayıcı sonunda sıfıra ulaşır ama daha aşağı saymaz', () => {})
+  test('[6] sayici.asagiSay ilk çağırılışında başlangıç sayışını yapıyor', () => {
+    const actual = sayici.asagiSay()
+    expect(actual).toBe(3);
+  })
+  test('[7] sayici.asagiSay İKİNCİ çağırılışında başlangıç eksi 1 sayıyor', () => {
+    sayici.asagiSay();
+    const actual = sayici.asagiSay();
+    expect(actual).toBe(2);
+  })
+  test('[8] sayıcı sonunda sıfıra ulaşır ama daha aşağı saymaz', () => {
+    for(let i = 0; i<100;i++){
+      sayici.asagiSay();
+    }
+    const actual = sayici.asagiSay();
+    expect(actual).toBe(0);
+  })
 })
 
 describe('[Görev 5] Mevsimler', () => {
@@ -34,12 +63,43 @@ describe('[Görev 5] Mevsimler', () => {
   beforeEach(() => {
     mevsimler = new utils.Mevsimler() // her test yeni bir mevsimle başlar
   })
-  // test('[9] mevsimler.sonraki İLK çağırılışında "yaz" döndürüyor', () => {})
-  // test('[10] mevsimler.sonraki İKİNCİ çağırılışında "sonbahar" döndürüyor', () => {})
-  // test('[11] mevsimler.sonraki ÜÇÜNCÜ çağırılışında "kış" döndürüyor', () => {})
-  // test('[12] mevsimler.sonraki DÖRDÜNCÜ çağırılışında "ilkbahar" döndürüyor', () => {})
-  // test('[13] mevsimler.sonraki BEŞİNCİ çağırılışında "yaz" döndürüyor', () => {})
-  // test('[14] mevsimler.sonraki KIRKINCI çağırılışında "ilkbahar" döndürüyor', () => {})
+  test('[9] mevsimler.sonraki İLK çağırılışında "yaz" döndürüyor', () => {
+    const actual = mevsimler.sonraki();
+    expect(actual).toBe("yaz");
+  })
+  test('[10] mevsimler.sonraki İKİNCİ çağırılışında "sonbahar" döndürüyor', () => {
+    mevsimler.sonraki();
+    const actual = mevsimler.sonraki();
+    expect(actual).toBe("sonbahar");
+  })
+  test('[11] mevsimler.sonraki ÜÇÜNCÜ çağırılışında "kış" döndürüyor', () => {
+    for(let i=0; i<2; i++){
+      mevsimler.sonraki();
+    }
+    const actual = mevsimler.sonraki();
+    expect(actual).toBe("kış");
+  })
+  test('[12] mevsimler.sonraki DÖRDÜNCÜ çağırılışında "ilkbahar" döndürüyor', () => {
+    for(let i=0; i<3; i++){
+      mevsimler.sonraki();
+    }
+    const actual = mevsimler.sonraki();
+    expect(actual).toBe("ilkbahar");
+  })
+  test('[13] mevsimler.sonraki BEŞİNCİ çağırılışında "yaz" döndürüyor', () => {
+    for(let i=0; i<4; i++){
+      mevsimler.sonraki();
+    }
+    const actual = mevsimler.sonraki();
+    expect(actual).toBe("yaz");
+  })
+  test('[14] mevsimler.sonraki KIRKINCI çağırılışında "ilkbahar" döndürüyor', () => {
+    for(let i=0; i<39; i++){
+      mevsimler.sonraki();
+    }
+    const actual = mevsimler.sonraki();
+    expect(actual).toBe("ilkbahar");
+  })
 })
 
 describe('[Görev 6] Araba', () => {
@@ -47,13 +107,37 @@ describe('[Görev 6] Araba', () => {
   beforeEach(() => {
     focus = new utils.Araba('focus', 20, 30) // her test yeni bir araba oluşturur
   })
-  // test('[15] arabayı sürünce güncellenmiş odometer döndürüyor', () => {})
-  // test('[16] arabayı sürmek benzin tüketiyor', () => {})
-  // test('[17] benzinalma arabayı sürmeye izin veriyor', () => {})
-  // test('[18] dolu depoya benzin alma etki etmiyor', () => {})
+  test('[15] arabayı sürünce güncellenmiş odometer döndürüyor', () => {
+    expect(focus.sur(100)).toBe(100);
+    expect(focus.sur(100)).toBe(200);
+    expect(focus.sur(300)).toBe(500);
+    expect(focus.sur(300)).toBe(600);
+  })
+  test('[16] arabayı sürmek benzin tüketiyor', () => {
+    focus.sur(300);
+    expect(focus.depo).toBe(10);
+  })
+  test('[17] benzinalma arabayı sürmeye izin veriyor', () => {
+    focus.sur(150);
+    focus.sur(450);
+    focus.benzinal(10);
+    focus.sur(100);
+    expect(focus.odometer).toBe(700);
+  })
+  test('[18] dolu depoya benzin alma etki etmiyor', () => {
+    focus.benzinal(10);
+    expect(focus.depo).toBe(20);
+  })
 })
 
-describe('[Görev 7] asenkronCiftSayi', () => {
-  // test('[19] bir çift sayı verilirse true çözümlüyor', () => {})
-  // test('[20] tek sayı verilirse false çözümlüyor', () => {})
-})
+describe("[Görev 7] asenkronCiftSayi", () => {
+  test("[19] bir çift sayı verilirse true çözümlüyor", () => {
+    utils.asenkronCiftSayi(2).then(res => {
+      expect(res).toBe(true);
+    })
+    //teste failed sorunlar çıkarır
+  });
+  test("[20] tek sayı verilirse false çözümlüyor", async () => {
+    expect(await utils.asenkronCiftSayi(3)).toBe(false);
+  });
+});
